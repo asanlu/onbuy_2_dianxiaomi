30359b2f311d108b2a4d7b550d63b0ca935ab4f9
luss <1925751694@qq.com>
2023-03-01 22:54:16 +0800

    修改列表生成方式

--- do.py
+++ do.py
@@ -6,7 +6,7 @@ dianxiaomi_col = ['*订单号', '*店铺账号', '*sku',	"属性(可填写SKU尺
                   '*单价', '总运费', '币种（默认USD）', '*买家姓名',	'*地址1',	'地址2',	'*城市',	'*省/州',	'*国家二字码',	'*邮编',	'电话',]
 # 获取onbuy中需要的数据
 onbuy_col = ['Order Number', 'SKU', 'Quantity', 'Product Unit Price', 'Customer', 'Delivery Address Name',
-             'Delivery Address Line 1', 'Delivery Address Line 2', 'Delivery Address Town', 'Delivery Address County', 'Site', 'Delivery Address Postcode']
+             'Delivery Address Line 1', 'Delivery Address Line 2', 'Delivery Address Line 3', 'Delivery Address Town', 'Delivery Address County', 'Site', 'Delivery Address Postcode']
 
 # 处理customer字段藏手机号
 
@@ -21,13 +21,19 @@ def get_onbuy_data(path=''):
     if not path:
         return
     # 店面名
-    f_dm = path.split('.')[0] or 'XMSTnew'
+    s_path = path.split('.')
+    f_dm = s_path[0] or 'XMSTnew'
+    print('====', path, s_path[-1])
 
-    # 读取excel
-    df = pd.read_excel(r'./'+path,
-                       usecols=onbuy_col,
-                       converters={'Customer': split_customer})
-    # print(df, '\n  -------')
+    if path.endswith('.xls'):
+        # 读取excel
+        df = pd.read_excel(r'./'+path, usecols=onbuy_col,
+                           converters={'Customer': split_customer})
+    elif path.endswith('.csv'):
+        df = pd.read_csv(r'./'+path, usecols=onbuy_col,
+                         converters={'Customer': split_customer}, encoding='utf-8')
+    # df = pd.concat([df1, df2])
+    print(df, '\n  -------')
     # df_li = df.values.tolist()
     # print(df_li, '\n -----------')
 
@@ -35,6 +41,36 @@ def get_onbuy_data(path=''):
     df_length = len(df)
     # 空表头
     empt = ''*df_length
+    # 处理address、Delivery Address County
+    town = list(df['Delivery Address Town'])
+    county = list(df['Delivery Address County'])
+    address2 = list(df['Delivery Address Line 2'])
+    address3 = list(df['Delivery Address Line 3'])
+    # print(df.loc[1, []], '-=======----')
+    # print(town)
+
+    for i in range(df_length):
+        # 城市是否多个
+        if ',' in town[i]:
+            # 省/州是否空
+            if pd.isnull(county[i]):
+                county[i] = town[i].split(',')[-1]
+            else:
+                county[i] = town[i]
+            # 地址3是否空
+            if pd.isnull(address3[i]):
+                address3[i] = town[i].split(',')[0]
+                town[i] = town[i].split(',')[-1]
+        else:
+            # 省/州是否空
+            if pd.isnull(county[i]):
+                county[i] = town[i]
+
+        # 地址3和地址2处理叠加不为空
+        if pd.isnull(address2[i]): address2[i]=''
+        if pd.isnull(address3[i]): address3[i]=''
+        address2[i] = (address2[i]+','+address3[i]).strip(',')
+
     # 处理表头数据
     dianxiaomi_li = {
         '*订单号': list(df['Order Number']),
@@ -47,10 +83,10 @@ def get_onbuy_data(path=''):
         '币种（默认USD）': empt,
         '*买家姓名': list(df['Delivery Address Name']),
         '*地址1': list(df['Delivery Address Line 1']),
-        '地址2': list(df['Delivery Address Line 2']),
-        '*城市': list(df['Delivery Address Town']),
-        '*省/州': list(df['Delivery Address County']),
-        '*国家二字码': list(df['Site']),
+        '地址2': address2,
+        '*城市': town,
+        '*省/州': county,
+        '*国家二字码': ['UK']*df_length,
         '*邮编': list(df['Delivery Address Postcode']),
         '电话': list(df['Customer']),
         '手机': empt,
@@ -76,17 +112,25 @@ def get_onbuy_data(path=''):
     return pd.DataFrame(dianxiaomi_li)
     # df = pd.DataFrame(df_li, columns=dianxiaomi_col)
 
+
 # 生成目标文件
 def get_dist_data():
     count_data = pd.DataFrame()
     file_list = get_file()
     # print(file_list)
+    o_name = file_list[0].split('.')[0]
     for i in file_list:
-        count_data = pd.concat([count_data,get_onbuy_data(i)])
+        count_data = pd.concat([count_data, get_onbuy_data(i)])
+
+        # try:
+        #     os.remove(i)
+        #     print(f'{i}文件删除完毕')
+        # except (FileNotFoundError):
+        #     print('文件不存在')
 
     # print('====', count_data)
-    count_data.to_excel("店小秘格式.xls", index=False)
-    print('生成店小秘格式文件')
+    count_data.to_excel(o_name+"店小秘订单.xls", index=False)
+    print(f'生成{o_name}店小秘订单文件')
 
 
 # 扫描xls文件
@@ -95,18 +139,31 @@ def get_file(path='./'):
     files = os.listdir(path)
     # files = os.walk(path)
     xls = []
-    if not len(files): 
+    if not len(files):
         print('目录为空，不执行操作\n')
 
     for f in files:
         f_name = f.split('.')
-        if (f_name[0] != '店小秘格式' and f_name[-1] == 'xls'):
+        # if (f_name[-1] == 'csv' or f_name[-1] == 'xls'):
+        if (f_name[-1] == 'csv'):
             xls.append(f)
             print('待处理xls文件：', os.path.join(path, f))
     return xls
 
 
+# 判断文件是否以打开
+def file_is_openState(file_path):
+    try:
+        print(open(file_path, "w"))
+        return False
+    except Exception as e:
+        if ("[Errno 13] Permission denied" in str(e)):
+            print("文件已打开!")
+            return True
+        else:
+            return False
+
+
 if __name__ == '__main__':
-    # get_file()
     get_dist_data()
-    input('按回车退出…')
+    # input('按回车退出…')
